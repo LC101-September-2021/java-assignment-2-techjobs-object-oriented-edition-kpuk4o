eb391bfadedff381297667d8e4c89d49d91cdb42
kpuk4o <kpuk4o@gmail.com>
2022-02-07 18:19:07 -0600

    Test the empty constructor

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,40 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobId(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertEquals(job1.getId()+1, job2.getId());
+        assertFalse(job1.getId() == job2.getId());
+        assertTrue(job1.getId() != job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals(job.getName(), "Product tester");
+        assertTrue(job.getName() instanceof String);
+        assertEquals(job.getEmployer().getValue(), "ACME");
+        assertTrue(job.getEmployer() instanceof Employer);
+        assertEquals(job.getLocation().getValue(), "Desert");
+        assertTrue(job.getLocation() instanceof Location);
+        assertEquals(job.getPositionType().getValue(), "Quality control");
+        assertTrue(job.getPositionType() instanceof PositionType);
+        assertEquals(job.getCoreCompetency().getValue(), "Persistence");
+        assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+    }
 
 }
